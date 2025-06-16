04c69cbafb7887953e15141971f34af675196c8a
Arath Márquez <arathmkz10@gmail.com>
2025-06-16 15:10:51 -0700

    added admin site and new models

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -7,8 +7,21 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
-def __str__(self):
-    """Return a string representing of the model"""
-    return self.text
+    def __str__(self):
+        """Return a string representing of the model"""
+        return self.text
 
+
+class Entry(models.Model):
+    """An entry an user could register about a topic"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a simple string representing the entry"""
+        return f"{self.text[:10]}.... related to {self.topic}."
 
