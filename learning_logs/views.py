061ff73bd2e682d8b69eabfacc0e2590cacb8d43
Arath Márquez <arathmkz10@gmail.com>
2025-07-01 16:00:46 -0700

    created the template for each topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -14,5 +14,14 @@ def topics(request):
 
     return render(request, 'learning_logs/topics.html', context)
 
-def topic(request,):
+def topic(request, topic_id):
     """page for each topic"""
+
+    topic = Topic.objects.get(id=topic_id)
+
+    entries = topic.entry_set.order_by('-date_added')
+    
+    context = {'topic': topic,
+               'entries': entries}
+    
+    return render(request, 'learning_logs/topic.html', context)
